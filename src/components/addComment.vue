a4c61db17bb304b6ee34f4a417b1583e0205eac4
Murillo Pinheiro de Oliveria <83728277+MuriWolf@users.noreply.github.com>
2022-10-31 22:34:17 -0300

    add comment update

--- src/components/addComment.vue
+++ src/components/addComment.vue
@@ -1,5 +1,9 @@
 <template>
-  <div></div>
+  <div class="add-comment">
+    <img class="comment__profile__img" src="../assets/avatars/image-amyrobson.png" alt="">
+    <input type="text" class="add-comment-input input">
+    <button class="btn btn--blue ">send</button>
+  </div>
 </template>
 
 <script lang="ts">
