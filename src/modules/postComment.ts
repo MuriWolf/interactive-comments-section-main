79a6adfc9ce7e2bfa15cfbf438365a6653696d7a
Murillo Pinheiro de Oliveria <murillop.o06@gmail.com>
2022-11-08 16:41:37 -0300

    update

--- src/modules/postComment.ts
+++ src/modules/postComment.ts
@@ -1,26 +1,31 @@
 // import Comment from "@/types/Comment"
+import CommentType from "@/types/Comment";
 import User from "@/types/User";
 import getData from "./getData";
 
 function postComment(url: string, commentContent: string) {
   getData<User>("http://localhost:3000/currentUser").then(data => {
     const currentUser: User = data
-    fetch(url, {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json, text/plain, */*',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        id: 6,
-        content: commentContent,
-        createdAt: "Right now",
-        score: 0,
-        user: currentUser,
-        replies: []
-      })
-    }).then(res => res.json())
-  })
+    getData<CommentType[]>("http://localhost:3000/comments").then(data => {
+      const comments = data;
+      fetch(url, {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json, text/plain, */*',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          id: 6,
+          content: commentContent,
+          createdAt: "Right now",
+          score: 0,
+          user: currentUser,
+          replies: []
+        })
+      }).then(res => res.json())
+    })
+    })
+
 }
 
 export default postComment;
